df2607846d1e9459832434bc6559b010d7354c95
liuyanbo20061020-cloud <liuyanbo20061020@gmail.com>
2026-09-21 18:44:58 +0800

    try to add a companion

---
+++ themes/yerbery/source/js/companions.js
@@ -0,0 +1,5 @@
+(() => {
+  // v15 intentionally keeps Yunxiao quiet.
+  // No blink/wink, no dialogue, no dragging.
+  // Motion is handled purely by CSS for a calmer "living in the page" effect.
+})();
